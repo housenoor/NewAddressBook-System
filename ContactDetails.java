fe07b7c96c41b5d474a075f48415d3ce2d8ad3d6
Srikanth <srikanthh994@gmail.com>
2021-11-10 11:26:49 +0530

    Added:Sorting method By stream Concept

--- ContactDetails.java
+++ ContactDetails.java
@@ -1,21 +1,28 @@
 package com.bl.addressbook;
 
-
-import java.util.ArrayList;
-
-public abstract class ContactDetails {
-	ArrayList<String> contactDetails = new <String>ArrayList(4);
+public class ContactDetails {
 
 	private String firstName;
 	private String lastName;
-	private String address;
-	private String city;
+	private String addressCity;
 	private String state;
 	private String email;
-	private String phoneNumber;
 	private String zip;
+	private String phoneNumber;
+
+	ContactDetails(String firstName, String lastName, String addressCity, String state, String email, String zip,
+			String phoneNumber) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.addressCity = addressCity;
+		this.state = state;
+		this.email = email;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+	}
 
 	public String getFirstName() {
+
 		return firstName;
 	}
 
@@ -24,38 +31,35 @@ public abstract class ContactDetails {
 	}
 
 	public String getLastName() {
+
 		return lastName;
 	}
 
 	public void setLastName(String lastName) {
+
 		this.lastName = lastName;
 	}
 
-	public String getAddress() {
-		return address;
+	public String getAddressCity() {
+		return addressCity;
 	}
 
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
+	public void setAddressCity(String addressCity) {
+		this.addressCity = addressCity;
 	}
 
 	public String getState() {
+
 		return state;
 	}
 
 	public void setState(String state) {
+
 		this.state = state;
 	}
 
 	public String getEmail() {
+
 		return email;
 	}
 
@@ -63,26 +67,29 @@ public abstract class ContactDetails {
 		this.email = email;
 	}
 
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-
 	public String getZip() {
 		return zip;
 	}
 
 	public void setZip(String zip) {
+
 		this.zip = zip;
+	}
+
+	public String getPhoneNumber() {
+
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+
+		this.phoneNumber = phoneNumber;
 	}
 
 	@Override
 	public String toString() {
-		return "PersonDetail [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city="
-				+ city + ", state=" + state + ", email=" + email + ", phoneNumber=" + phoneNumber + ", zip=" + zip
-				+ "]";
+		return "ContactDetails{" + "firstName='" + firstName + '\'' + ", lastName='" + lastName + '\''
+				+ ", addressCity='" + addressCity + '\'' + ", state='" + state + '\'' + ", email='" + email + '\''
+				+ ", zip=" + zip + ", phoneNumber=" + phoneNumber + '}';
 	}
 }
